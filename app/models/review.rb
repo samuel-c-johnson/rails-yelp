4d548687a542c556b84e4312a8ce2af82abf48cf
Eleanor Wintram <eleanorwintram@gmail.com>
2018-01-19 16:27:35 +0000

    users can only make one review per restaurant

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true,
                      numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5}
+  validates :user_id, uniqueness: { scope: :restaurant_id, message: "Users can only write one review per restaurant" }
 end
